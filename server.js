82129b50ae2163c7cd701412910ae61d8cd6cec7
Visnu Pitiyanuvath <visnupx@gmail.com>
2010-09-30 00:29:39 -0700

    initial working commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express'),
+    io = require('socket.io'),
+    pub = __dirname + '/public';
+
+var app = express.createServer(
+  express.compiler({ src: pub, enable: ['sass'] }),
+  express.staticProvider(pub),
+  express.logger(),
+  express.errorHandler({ dumpExceptions: true, showStack: true })
+);
+
+app.get('/', function(req, res) {
+  res.render('index.jade');
+});
+
+var socket = io.listen(app);
+socket.on('connection', function(client) {
+  var spawn = require('child_process').spawn,
+      ssh = spawn('ssh', ['-t', '-t', 'theglamourist.com']);
+
+  ssh.stderr.on('data', function(data) { client.send(data); });
+  ssh.stdout.on('data', function(data) { client.send(data); });
+
+  client.on('message', function(data) {
+    if (data === '\r') data = '\n';
+    ssh.stdin.write(data);
+  });
+
+  client.on('disconnect', function() {
+    ssh.kill();
+  });
+});
+
+app.listen(process.env.PORT || 8000);
