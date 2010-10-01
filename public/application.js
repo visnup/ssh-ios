4a61126b9e3c899209252ef2a018be2f0102af73
Visnu Pitiyanuvath <visnupx@gmail.com>
2010-09-30 23:30:51 -0700

    escape html

--- public/application.js
+++ public/application.js
@@ -7,7 +7,7 @@ $(function() {
       esc = new RegExp(String.fromCharCode(27) + '\\[([0-9;]*?m)', 'g');
   socket.on('message', function(data) {
     var spans = 0;
-    data = data.replace(esc, function(m, p1) {
+    data = $('<div>').text(data).html().replace(esc, function(m, p1) {
       spans++;
       return '<span class="e' + p1.replace(/[;m]/g, ' e') + '">';
     });
