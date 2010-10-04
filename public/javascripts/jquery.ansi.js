e3f1a25cfda2deacd4ed743f21f690db944ca59c
Visnu Pitiyanuvath <visnupx@gmail.com>
2010-10-03 17:55:21 -0700

    handle xterm title sequences

--- public/javascripts/jquery.ansi.js
+++ public/javascripts/jquery.ansi.js
@@ -1,13 +1,27 @@
 (function($, window, document, undefined) {
-  var esc = new RegExp(String.fromCharCode(27) + '\\[([0-9;]*?m)', 'g');
+  var xterm = new RegExp('\033][0-9];(.*?)\007', 'g'),
+      esc = new RegExp('\033\\[([0-9;]*?m)', 'g');
+
+  var debug = function(s) {
+    return;
+    console.log(s.split('').map(function(c) {
+      return c + '=' + c.charCodeAt(0);
+    }));
+  };
 
   $.ansi = {
-      colorize: function(data) {
+    colorize: function(data) {
       var spans = 0;
-      data = $('<div>').text(data).html().replace(esc, function(m, p1) {
-        spans++;
-        return '<span class="e' + p1.replace(/[;m]/g, ' e') + '">';
-      });
+      debug(data);
+      data = $('<div>').text(data).html()
+      .replace(xterm, function(m, p1) {
+          $('title').text(p1);
+          return '';
+        })
+        .replace(esc, function(m, p1) {
+          spans++;
+          return '<span class="e' + p1.replace(/[;m]/g, ' e') + '">';
+        });
       for (var i = 0; i < spans; i++)
         data += '</span>';
 
